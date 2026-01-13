24d428bc4b8292e849c9d07d1971445da6aff235
Tri Kandi <2511530018@mahasiswa.atmaluhur.ac.id>
2026-01-12 20:13:06 -0800

    mengubah localhost

--- pertemuan-10/koneksi.php
+++ pertemuan-10/koneksi.php
@@ -3,9 +3,9 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "db_pwd2025" ;
-$conn = mysqli_connect($host,$user,$pass,$db);
+$db   = "db_pwd" ;
+$conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
-    die("koneksi gagal" . mysqli_connect_error())
+    die("koneksi gagal" . mysqli_connect_error());
 }
